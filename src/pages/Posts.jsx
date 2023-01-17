4c352862f370d82d4f229c9049184f39e3cc268f
Liam <backgroundpositioncenter@gmail.com>
2023-01-17 03:42:34 +0300

    added infinite feed and dynamic pagination

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import '../app/styles/index.css';
 import {PostList} from "../components/PostList";
@@ -10,6 +10,8 @@ import {usePosts} from "../hooks/usePosts";
 import PostService from "../API/PostService";
 import {getPageCount} from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import {useObserver} from "../hooks/useObserver";
+import {MySelect} from "../components/UI/select/MySelect";
 
 const Posts = () => {
 
@@ -21,10 +23,15 @@ const Posts = () => {
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const lastElement = useRef();
+
+    useObserver(lastElement, isPostLoading, page < totalPages, () => {
+        setPage(page + 1);
+    })
 
     useEffect(() => {
         fetchPosts()
-    }, [page])
+    }, [page, limit])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -34,7 +41,7 @@ const Posts = () => {
     async function fetchPosts() {
         setIsPostLoading(true);
         const response = await PostService.getAll(limit, page)
-        setPosts(response.data)
+        setPosts([...posts, ...response.data])
         const totalCount = response.headers['x-total-count']
         setTotalPages(getPageCount(totalCount, limit))
         setIsPostLoading(false);
@@ -57,9 +64,21 @@ const Posts = () => {
             </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            {isPostLoading ?
-                <h1 style={{display: 'flex', justifyContent: 'center', marginTop: '60px'}}>Загрузка постов...</h1> :
-                <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post's List"/>
+            <MySelect
+                value={limit}
+                onChange={value => setLimit(value)}
+                defaultName='Количество элементов на странице'
+                options={[
+                    {value: 3, name: '3'},
+                    {value: 5, name: '5'},
+                    {value: 15, name: '15'},
+                    {value: -1, name: 'Показать все'},
+                ]}
+            />
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post's List"/>
+            <div ref={lastElement} style={{height: 20, background: 'red'}}></div>
+            {isPostLoading &&
+                <h1 style={{display: 'flex', justifyContent: 'center', marginTop: '60px'}}>Загрузка постов...</h1>
             }
             <Pagination
                 page={page}
